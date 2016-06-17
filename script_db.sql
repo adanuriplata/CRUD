1f89a9c0fc81b9e2eb94dc6a885108daef8f90e2
Adan Uri Plata Estrada <Adan Uri Plata Estrada>
2016-06-17 18:17:54 -0500

    estructura de inicio

---
+++ script_db.sql
@@ -0,0 +1,13 @@
+create database CRUD default character set utf8;
+
+	create table secciones (id int(11) auto_increment not null primary key,
+							nombre varchar(80) not null,
+							);
+
+	create table estudiantes (id int(11) auto_increment not null primary key,
+							nombre varchar(200) not null,
+							edad int(11) not null,
+							promedio double not null,
+							imagen varchar(200) not null,
+							id_seccion int(11) not null,
+							 );
